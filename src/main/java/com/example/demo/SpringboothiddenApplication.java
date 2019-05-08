bec9c76f3cd67eb422f017adf5dedc8d9b50da26
Naresh Ganapavaram Lakshmi <glnaresh@gmail.com>
2019-05-08 20:25:31 +0200

    annoating hidden service

--- src/main/java/com/example/demo/SpringboothiddenApplication.java
+++ src/main/java/com/example/demo/SpringboothiddenApplication.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan({"com.springboothidden.controller"})
 public class SpringboothiddenApplication {
 
